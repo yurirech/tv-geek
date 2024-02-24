c97b9708797c440fdbe16467d0f84e3a2ae08868
Yuri Rech <yurn89@gmail.com>
2024-02-24 16:29:47 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
 	return (
 		<>
-			<div className="flex text-center">
+			<div className="flex">
 				<a href="https://vitejs.dev" target="_blank">
 					<img src={viteLogo} className="logo" alt="Vite logo" />
 				</a>
