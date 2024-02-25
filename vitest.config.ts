e46a52e2d1ff47c8ed8efaa6549346a0f9f69263
Yuri Rech <yurn89@gmail.com>
2024-02-25 12:44:18 +0100

    chore: setup tests with vitest

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig(({ mode }) => ({
+  plugins: [react()],
+  // resolve: {
+  //   conditions: mode === 'test' ? ['browser'] : [],
+  // },
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./vitest-setup.ts'],
+    globals: true
+  },
+}))
