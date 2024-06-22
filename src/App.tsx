6464411138416d141e2fb906a2d36087f6f09474
Yuri Rech <yurn89@gmail.com>
2024-06-22 11:12:18 +0200

    Instal Helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import "./App.css";
 import Landing from "./pages/Landing";
-
+import { Helmet } from "react-helmet";
 function App() {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Geek TV</title>
+      </Helmet>
       <Landing />
     </div>
   );
